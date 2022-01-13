7901506191f00027244f1d524329de44d077224a
Glitch (hello-express) <none>
2022-01-13 23:03:43 +0000

    🎩🐃 Checkpoint ./config.json:26026384/59 ./package.json:26026384/289 ./package-lock.json:26026384/4419 ./index.js:26026384/810

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const { Client, version } = require("discord.js");
-const { token } = require("./config.json");
+const { Client, version } = require('discord.js');
+const { 
+    token
+} = require('./config.json')
 const bot = new Client();
 
-bot.on("ready", async () => {
-  console.log(`[ Client ] ${bot.user.tag} Is Now Online`);
-
+bot.on("ready", async() => {
+    console.log(`[ Client ] ${bot.user.tag} ESTA ONLINE AGORA`);
+  
   bot.user.setPresence({
-    status: "dnd",
-    activity: {
-      name: "glitch.me",
-      type: "PLAYING",
-    },
-  });
-});
+        status: 'dnd',
+        activity: {
+            name: 'CAÇA AOS RATOS DO QUINTAL',
+            type: 'PLAYING'        }
+    })
+})
+ 
 
-bot.login(token);
+bot.login(token)
