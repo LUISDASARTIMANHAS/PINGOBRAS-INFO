1b63df1543725e646e6a6ff77a3f8815a21f5296
Glitch (hello-express) <none>
2022-01-13 23:19:44 +0000

    🚏👍 Checkpoint ./index.js:26026384/307

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-const { Client, version } = require('discord.js');
-const express = require('express');
-
-const app = express();
-
-app.get("/", (request, response) => {
-
-const ping = new Date(); 
-
-  ping.setHours(ping.getHours() - 3);
-
-  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
-
-  response.sendStatus(200)
-
-});
-
-
+const { Client, version } = require('discord.js')
 const { 
     token
 } = require('./config.json')
