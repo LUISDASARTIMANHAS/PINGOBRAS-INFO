43b90ffb4abce67caa342051fb84e515dff87161
Glitch (hello-express) <none>
2022-06-05 15:47:58 +0000

    👬🔮 Checkpoint ./autentication.js:26026384/316

--- autentication.js
+++ autentication.js
@@ -3,15 +3,15 @@ let  JsonKeys = localStorage.getItem("JsonKeys");
 let key = JSON.parse(JsonKeys);
 console.log(key.senha);
 
-let password = document.querySelector("#senha")
+let password = document.getElementById("senha")
 console.log(password.value)
 
 function entrar(){
     
-  let msgError = document.querySelector('#msgError')
+  let msgError = document.getElementById("msgError")
     
-  if( password.value === key.senha & password.value == process.env.ADMIN_KEY){
-    window.location.href = 'https://codepen.io/luisdasartimanhas/full/ZEaVByy'
+  if( password.value === key.senha){
+    window.location.href = "https://codepen.io/luisdasartimanhas/full/ZEaVByy"
     
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
@@ -30,6 +30,6 @@ function entrar(){
 function refresh(){
   const adminKey = {senha: 2004};
 const CryptoKey = JSON.stringify(adminKey);
-localStorage.pingobras.glitch.me.setItem("JsonKeys", CryptoKey);
+localStorage.setItem("JsonKeys", CryptoKey);
 alert('O banco de dados foi restaurado!')
 }
