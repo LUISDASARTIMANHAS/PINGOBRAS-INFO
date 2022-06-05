ab9df490d8cb6eff11712bdcb8540591052bc957
Glitch (hello-express) <none>
2022-06-05 15:24:58 +0000

    🎙🍂 Checkpoint ./autentication.js:26026384/1129 ./autentication=user=true.html:26026384/1404 ./index.html:26026384/2203

---
+++ autentication.js
@@ -0,0 +1,38 @@
+function continuar() {
+window.location.href ="https://pingobras-autentication.glitch.me/autentication=user=true.html";
+}
+
+let  JsonKeys = localStorage.getItem("JsonKeys");
+let key = JSON.parse(JsonKeys);
+console.log(key.senha);
+
+let password = document.querySelector("#senha")
+console.log(password.value)
+
+function entrar(){
+    
+  let msgError = document.querySelector('#msgError')
+    
+  if( password.value === key.senha & password.value == process.env.ADMIN_KEY){
+    window.location.href = 'https://codepen.io/luisdasartimanhas/full/ZEaVByy'
+    
+    let mathRandom = Math.random().toString(16).substr(2)
+    let token = mathRandom + mathRandom
+    
+    localStorage.setItem('token', token)}
+  
+  else {
+    password.setAttribute('style', 'color: red')
+    password.setAttribute('style', 'border-color: red')
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+   
+  }
+}
+
+function refresh(){
+  const adminKey = {senha: 2004};
+const CryptoKey = JSON.stringify(adminKey);
+localStorage.setItem("JsonKeys", CryptoKey);
+alert('O banco de dados foi restaurado!')
+}
