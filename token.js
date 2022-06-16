64cc239d3664b7858caa64b11fb38b8f853b9864
Glitch (hello-express) <none>
2022-06-16 18:24:49 +0000

    🐏👩‍👩‍👦 Checkpoint ./token.js:26026384/335

--- token.js
+++ token.js
@@ -3,5 +3,10 @@ console.log(tokenStorage);
 console.log("token requerida!");
 
 let userID = document.getElementById("userID");
-
  userID.innerHTML = tokenStorage;
+
+
+const CryptoToken = JSON.stringify(tokenStorage);
+console.log("codificando token");
+localStorage.setItem("JsonToken", CryptoToken);
+console.log("token redefinido");
