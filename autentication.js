6ba21b095c035fda0f550f16a9fb6867277a62cf
Glitch (hello-express) <none>
2022-06-16 17:16:11 +0000

    🌚☎️ Checkpoint ./autentication.js:26026384/947 ./index.html:26026384/149

--- autentication.js
+++ autentication.js
@@ -7,20 +7,25 @@ let inputsenha = document.getElementById("senha")
 console.log(inputsenha.value)
 
 let msgError = document.getElementById("msgError");
+let msgSuccess = document.getElementById("msgSuccess");
+let userID = document.getElementById("userID");
+let  tokenJson = localStorage.getItem("token");
+let token = JSON.parse(JsonKeys);
+ userID.innerHTML = token ;
+
+
 
 var enviar = document.querySelector("#submit");
 enviar.addEventListener("click", function entrar() {  
-
-  
   alert('Verificando!...');
-  
-  if(inputsenha.value == key.senha) {
+   if(inputsenha.value == key.senha) {
     window.location.href = "https://codepen.io/luisdasartimanhas/full/ZEaVByy"
-    
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
-    
     localStorage.setItem('token', token)
+    
+    msgSuccess.setAttribute('style', 'display: block')
+    msgSuccess.innerHTML = 'senha correta! redirecionando!'
   }
   else{
     inputsenha.setAttribute('style', 'color: red')
